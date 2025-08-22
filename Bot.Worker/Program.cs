7e11d4d65273e6b928b8e86894cab60e04f4f75d
Kyron Meza <me@kyronmeza.com>
2025-08-21 22:08:24 -0400

    Added Serilog support for Bot.Worker. Fixed appsettings templates.

--- Bot.Worker/Program.cs
+++ Bot.Worker/Program.cs
@@ -1,7 +1,33 @@
 using Bot.Worker;
+using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
+// Add configuration or service dependencies here.
+ServicesConfiguration.RegisterServices(builder.Services, builder.Configuration);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Services.AddSerilog();
+
 var host = builder.Build();
-host.Run();
+
+try
+{
+    Log.Information("Bot.Worker starting up");
+    host.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Bot.Worker terminated unexpectedly");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
