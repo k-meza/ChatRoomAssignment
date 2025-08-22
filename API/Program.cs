d58d142a085605e9c71c051fdc6551634fc9aaa5
Kyron Meza <me@kyronmeza.com>
2025-08-21 21:48:37 -0400

    Added Serilog support and Separate DI Container

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add configuration or service dependencies here.
+ServicesConfiguration.RegisterServices(builder.Services, builder.Configuration);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -18,6 +31,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
